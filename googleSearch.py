bc6ad2f060f5b8c74d43fbfeff10832f28c5a140
Vivek <48525663+vivek-sheta@users.noreply.github.com>
2022-08-07 12:13:56 +0530

    Create googleSearch.py

---
+++ googleSearch.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import webbrowser
+
+user_search = input("Search: ")
+print("Googleing.........")
+
+string = "https://www.google.com/search?q=" + user_search
+user_agent = "Chrome/81.0.4044.129 Safari/537.36"
+
+request_search_page = requests.get(string)
+
+webbrowser.open(string)
+#print(soup.prettify())
