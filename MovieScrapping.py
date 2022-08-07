ea233d6dbe63ccb035a89d8867aa59c8355fdaf0
Vivek <48525663+vivek-sheta@users.noreply.github.com>
2022-08-07 12:12:17 +0530

    Create MovieScrapping.py

---
+++ MovieScrapping.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+page = requests.get(
+    "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+)
+soup = BeautifulSoup(page.content,'html.parser')
+print(soup)
+container = soup.find('tbody',class_="lister-list")
+
+title = [name.find('a').get_text() for name in container.find_all(class_="titleColumn")]
+release_year = [name.find('span').get_text() for name in container.find_all(class_="titleColumn")]
+rating = [name.find('strong').get_text() for name in container.find_all(class_="ratingColumn imdbRating")]
+
+top_rated_movies = pd.DataFrame({
+        "title" : title,
+        "Release Year" : release_year,
+        "rating" : rating,
+    })
+    
+print(top_rated_movies)
+top_rated_movies.to_csv("top_rated_movies.csv")
